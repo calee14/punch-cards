1122120b338662cd4e0d4c4a8e2677aca82e92f2
Cappillen Lee <cappillen@csilvm-19.cs.ucsb.edu>
2025-01-30 20:44:14 -0800

    tree sum

--- pa1/treesum_mpi.c
+++ pa1/treesum_mpi.c
@@ -26,6 +26,37 @@ int global_sum(int my_int /* in */, int my_rank /* in */, int comm_sz /* in */,
   int my_sum = my_int;
 
   /* Your solution */
+  int lrg_power_two = 1;
+  while (lrg_power_two*2 <= comm_sz)
+    lrg_power_two *= 2;
+  MPI_Status status;
+  
+  // make init tree size a power of 2
+  if(my_rank >= lrg_power_two && my_rank < comm_sz) {
+    MPI_Send(&my_sum, 1, MPI_INT, my_rank - lrg_power_two, 0, comm);
+  } else if (my_rank < comm_sz - lrg_power_two) {
+    int val;
+    MPI_Recv(&val, 1, MPI_INT, my_rank + lrg_power_two, 0, comm, &status);
+    my_sum += val;
+  }
+
+  if(my_rank >= lrg_power_two) {
+    return my_sum;
+  }
+
+  // tree sum algo
+  if((my_rank % 2 != 0 && my_rank > 0))
+    MPI_Send(&my_sum, 1, MPI_INT, my_rank - 1, 0, comm);
+  for(int gap=1; gap<lrg_power_two; gap *= 2) {
+    if(my_rank % (2 * gap) == 0) {
+      int other_sum;
+      MPI_Recv(&other_sum, 1, MPI_INT, my_rank + gap, 0, comm, &status);
+      my_sum = other_sum + my_sum;
+      if((my_rank % (4 * gap) != 0 && my_rank >= 2 * gap)) {
+        MPI_Send(&my_sum, 1, MPI_INT, my_rank - 2*gap, 0, comm);
+      }
+    }
+  }
 
   return my_sum;
 } /* Global_sum */
--- pa1/treesum_test_mpi.c
+++ pa1/treesum_test_mpi.c
@@ -28,13 +28,43 @@ int global_sum(int my_int, int my_rank, int no_proc, MPI_Comm comm);
  */
 char *treesum_test1() {
   /* Your solution */
-  return "Not tested";
+
+  // allocate memory and make rand list of int
+  int *local_list = malloc(no_proc * sizeof(int));
+  int all_succ;
+  int succ = local_list != NULL;
+  MPI_Allreduce(&succ, &all_succ, 1, MPI_INT, MPI_PROD, comm);
+  if (all_succ == 0) return "Memory allocation failed";
+
+  int my_int = rand() % 10;
+
+  MPI_Allgather(&my_int, 1, MPI_INT, local_list, 1, MPI_INT, comm);
+
+  // run prog in parallel
+  double startwtime = 0, endwtime = 0;
+  if (my_rank == 0) startwtime = MPI_Wtime();
+  int func_sum = global_sum(my_int, my_rank, no_proc, comm);
+  if (my_rank == 0) {
+    endwtime = MPI_Wtime();
+    printf("Test 1: Wall clock time = %f at Proc 0 of %d processes\n",
+           endwtime - startwtime, no_proc);
+    int real_sum = 0;
+    for(int i = 0; i < no_proc; i++) real_sum += local_list[i];
+    free(local_list);
+    if(func_sum != real_sum) return "Incorrect sum";
+  } else {
+    free(local_list);
+  }
+
+  return NULL;
 }
 
 /*-------------------------------------------------------------------
  * Run all tests.  Ignore returned messages.
  */
-void run_all_tests(void) { mu_run_test(treesum_test1); }
+void run_all_tests(void) { 
+  mu_run_test(treesum_test1);
+}
 
 /*-------------------------------------------------------------------
  * The main entrance to run all tests.
